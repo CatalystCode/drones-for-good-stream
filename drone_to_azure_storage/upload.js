39b44e9c468bba55dee58ab61ebee2b747d6bd2b
Elad Iwanir <eladiw@microsoft.com>
2017-07-25 09:23:28 +0300

    Adding building blocks to support complete e2e pipe from drone to azure blobs This commit contains the nginx config file that expose a RMTP endpoint. The endpoint records the video, passes it to another util which is also commited which exracts frames from the video using ffmpeg, uploads the frames to azure blob (and azure queues) and deletes the video

---
+++ drone_to_azure_storage/upload.js
@@ -0,0 +1,3 @@
+var videoUtils = require('./videoutils');
+
+videoUtils.processFile('/tmp/vc/' + process.argv[2], 'drones4goodstore', 'place holder for key');
---
+++ drone_to_azure_storage/videoutils.js
@@ -0,0 +1,104 @@
+var ffmpeg = require('ffmpeg');
+var azure = require('azure-storage');
+var fs = require('fs');
+
+processFile = (videoName, accountName, accountKey) => {
+  var process = new ffmpeg(videoName);
+  process.then(function (video) {
+    // Callback mode
+    video.fnExtractFrameToJPG('/home/eladiw/util/frames/', {
+      start_time: `0:00:01`,
+      frame_rate : 1, // a frame per second
+      file_name : `frm_%t_%s`
+    }, function (error, files) {
+      if (error) {
+        console.log('failed extracting frames:' + error);
+        return;
+      }
+      console.log('Frames: ' + files);
+       
+	    fs.unlink(videoName, function (error, files) {
+	           if (!error) {
+			           console.log('deleted file');
+			       }
+	           if (error) {
+			           console.log('error deleting file: ' + error);
+			       }
+      });
+
+      console.log('deleted vid file');
+      // creating perstistent storage elements (blob and queue):
+      var blobService = azure.createBlobService(accountName, accountKey);
+      var queueService = azure.createQueueService(accountName, accountKey);
+      blobService.createContainerIfNotExists(
+        'imagestoprocess', 
+        { 
+          publicAccessLevel: 'blob'
+        }, 
+        function(error, result, response) {
+          if (error) {
+            console.log('failed creating blob container');
+            return;
+          }
+        
+          console.log('have blob container');
+          queueService.createQueueIfNotExists(
+            'imagestoprocess', 
+            function(error, result, response) {
+              if(error) {
+                console.log('failed creating queue');
+                return;
+              }
+          
+              console.log('have queue');
+              handleFiles(files, queueService, blobService);
+          });
+      });
+  });
+}, function (err) {
+    console.log('Error: ' + err);
+});
+}
+
+deleteFile = (element) => {
+    fs.unlink(element, function (error, files) {
+    if (!error) {
+        console.log('deleted file');
+    }
+    if (error) {
+        console.log('error deleting file: ' + error);
+    }
+});
+};
+
+handleFiles = (files, queueService, blobService) => {
+  files.forEach(function(element) {
+    var fName = element.substr(element.lastIndexOf('/') + 1);
+    blobService.createBlockBlobFromLocalFile(
+      'imagestoprocess', 
+      fName, 
+      element, 
+      function(error, result, response) {
+        if (error) {
+            console.log('error creating blob item:' + error);
+        }
+        
+        // file uploaded. now lets save the message into the queue and delete the file
+        console.log('file saved into blob');
+        queueService.createMessage(
+          'imagestoprocess', 
+          fName, 
+          function(error) {
+            // Queue created or exists
+            if (error) {
+              console.log('error creating queue message:' + error);
+            }
+            
+            console.log('sent msg');
+            deleteFile(element);
+          });
+      });
+  });
+};
+
+var exports = module.exports = { processFile };
